a52d61e9e53e4a953b96983ba9b32bdf129a6187
Jung Hoan Kim <kjunghoan@gmail.com>
2023-07-14 18:35:21 -0400

    troubleshooting

--- src/seed.ts
+++ src/seed.ts
@@ -1,23 +1,25 @@
 import { PrismaClient } from "@prisma/client";
-
+import path from "path";
 
 const prisma = new PrismaClient();
 
-const pathToFile = `/home/kjunghoan/hackReactor/week8/QuestionsAndAnswers/src/database/`;
+const pathToFile = path.join(__dirname, "/database/");
 
 async function main() {
   //Write prisma client queries here
-  await prisma.$executeRaw `COPY Answers(id, question_id, body, date_written, answerer_name, answerer_email, reported, helpful)
-  FROM '${pathToFile}answers.csv'
+  // await prisma.$executeRaw`select * from "Answers"`
+  // console.dir(await prisma.$executeRaw`\dt`);
+  await prisma.$executeRaw `COPY "Answers"("id", "question_id", "body", "date_written", "answerer_name", "answerer_email", "reported", "helpful")
+  FROM '/home/kjunghoan/hackReactor/week8/QuestionsAndAnswers/src/database/answers.csv'
   DELIMITER ','
   CSV HEADER;`;
 
-  await prisma.$executeRaw `COPY Photos(id, answer_id, url)
+  await prisma.$executeRaw `COPY "Photos"("id", "answer_id", "url")
   FROM '${pathToFile}answers_photos.csv'
   DELIMITER ','
   CSV HEADER;`
 
-  await prisma.$executeRaw `COPY Questions(id, product_id, body, date_written, asker_name, asker_email, reported, helpful)
+  await prisma.$executeRaw `COPY "Questions"("id", "product_id", "body", "date_written", "asker_name", "asker_email", "reported", "helpful")
   FROM '${pathToFile}questions.csv'
   DELIMITER ','
   CSV HEADER;`;
