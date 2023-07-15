51887563424371fb6c79bdaa29be5ec6190d0feb
Jung Hoan Kim <kjunghoan@gmail.com>
2023-07-14 20:04:13 -0400

    Fixing seeding file

--- src/seed.ts
+++ src/seed.ts
@@ -4,17 +4,16 @@ const prisma = new PrismaClient();
 
 async function main() {
   //Write prisma client queries here
-  // await prisma.$executeRaw`select * from "Answers"`
-  // console.dir(await prisma.$executeRaw`\dt`);
-  // await prisma.$executeRaw `COPY "Questions"("id", "product_id", "body", "date_written", "asker_name", "asker_email", "reported", "helpful")
-  // FROM '/home/kjunghoan/hackReactor/week8/QuestionsAndAnswers/src/database/questions.csv'
-  // DELIMITER ','
-  // CSV HEADER;`;
 
-  // await prisma.$executeRaw `COPY "Answers"("id", "question_id", "body", "date_written", "answerer_name", "answerer_email", "reported", "helpful")
-  // FROM '/home/kjunghoan/hackReactor/week8/QuestionsAndAnswers/src/database/answers.csv'
-  // DELIMITER ','
-  // CSV HEADER;`;
+  await prisma.$executeRaw `COPY "Questions"("id", "product_id", "body", "date_written", "asker_name", "asker_email", "reported", "helpful")
+  FROM '/home/kjunghoan/hackReactor/week8/QuestionsAndAnswers/src/database/questions.csv'
+  DELIMITER ','
+  CSV HEADER;`;
+
+  await prisma.$executeRaw `COPY "Answers"("id", "question_id", "body", "date_written", "answerer_name", "answerer_email", "reported", "helpful")
+  FROM '/home/kjunghoan/hackReactor/week8/QuestionsAndAnswers/src/database/answers.csv'
+  DELIMITER ','
+  CSV HEADER;`;
 
   await prisma.$executeRaw `COPY "Photos"("id", "answer_id", "url")
   FROM '/home/kjunghoan/hackReactor/week8/QuestionsAndAnswers/src/database/answers_photos.csv'
