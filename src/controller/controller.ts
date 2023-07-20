a824de51837204f271c30244e15c77c1830ac965
Jung Hoan Kim <kjunghoan@gmail.com>
2023-07-19 23:45:56 -0400

    finish askQuestion in controller.ts

--- src/controller/controller.ts
+++ src/controller/controller.ts
@@ -42,13 +42,19 @@ export const getQuestionsByProduct = async(request:Request, response:Response)=>
 
 //gets a list of questions based off of question_id
 export const getAnswers = async(request:Request, response:Response) => {
-  const questionId: number = parseInt(request.params.id, 10);
+  const errors = validationResult(request);
+  if(!errors.isEmpty()) {
+    return response.status(400).json({errors: errors.array()});
+  }
   try{
-    const answers = await model.getAnswersByQuestion(questionId);
+    const question_id: number = parseInt(request.params.product_id);
+    const page: number = Number(request.query.page);
+    const count: number = Number(request.query.count);
+    const answers = await model.getAnswersByQuestion(question_id, page, count);
     if(answers) {
       return response.status(200).json(answers);
     } else {
-      return response.status(404).json(`Question # ${questionId} could not be found.`);
+      return response.status(404).json(`Question # ${question_id} could not be found.`);
     }
   } catch(error:any) {
     return response.status(500).json({
@@ -82,8 +88,12 @@ export const askQuestion = async(request: Request, response: Response) => {
 
 //posted an answer to database
 export const answerQuestion = async(request:Request, response:Response) => {
-  const newAnswer = request.body;
+  const errors = validationResult(request);
+  if(!errors.isEmpty()) {
+    return response.status(400).json({errors: errors.array()});
+  }
   try {
+    const newAnswer = request.body;
     const postedAnswer = await model.answerQuestion(newAnswer);
     if(postedAnswer) {
       return response.status(201).json(`answer question controller worked`);
@@ -99,8 +109,12 @@ export const answerQuestion = async(request:Request, response:Response) => {
 };
 //sends a put to increase question helpful
 export const updateQuestionHelpfulById = async(request:Request, response:Response) => {
-  const questionId: number = parseInt(request.params.id, 10);
+  const errors = validationResult(request);
+  if(!errors.isEmpty()) {
+    return response.status(400).json({errors: errors.array()});
+  }
   try {
+    const questionId: number = parseInt(request.params.id, 10);
     const question = await model.updateQuestionHelpful(questionId);
     if (question) {
       return response.status(202).json(question);
@@ -117,8 +131,12 @@ export const updateQuestionHelpfulById = async(request:Request, response:Respons
 
 //sends a put to report a question
 export const reportQuestionById = async(request:Request, response:Response) => {
-  const questionId: number = parseInt(request.params.question_id);
+  const errors = validationResult(request);
+  if(!errors.isEmpty()) {
+    return response.status(400).json({errors: errors.array()});
+  }
   try {
+    const questionId: number = parseInt(request.params.question_id);
     const question = await model.reportQuestion(questionId);
     if(question) {
       return response.status(202).json(question);
@@ -135,8 +153,12 @@ export const reportQuestionById = async(request:Request, response:Response) => {
 
 //sends a put to increase answer helpful
 export const updateAnswerHelpfulById = async(request:Request, response:Response) => {
-  const answerId: number = parseInt(request.params.id);
+  const errors = validationResult(request);
+  if(!errors.isEmpty()) {
+    return response.status(400).json({errors: errors.array()});
+  }
   try {
+    const answerId: number = parseInt(request.params.id);
     const answer = await model.updateAnswerHelpful(answerId);
     if(answer) {
       return response.status(202).json(answer);
@@ -153,8 +175,12 @@ export const updateAnswerHelpfulById = async(request:Request, response:Response)
 
 //sends a put to report an answer
 export const reportAnswerById = async(request:Request, response:Response) => {
-  const answerId: number = parseInt(request.params.answer_id);
+  const errors = validationResult(request);
+  if(!errors.isEmpty()) {
+    return response.status(400).json({errors: errors.array()});
+  }
   try {
+    const answerId: number = parseInt(request.params.answer_id);
     const answer = await model.reportAnswer(answerId);
     if(answer) {
       return response.status(202).json(answer);
