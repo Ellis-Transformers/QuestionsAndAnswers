79e2ab1c2cebf7b1ed4422a856a7167db71caa01
Jung Hoan Kim <kjunghoan@gmail.com>
2023-07-22 14:32:14 -0400

    fixing things up for docker

--- prisma/migrations/20230722182607_database_model/migration.sql
+++ prisma/migrations/20230722182607_database_model/migration.sql
@@ -1,11 +1,11 @@
 -- CreateTable
 CREATE TABLE "Questions" (
     "id" SERIAL NOT NULL,
-    "product_id" INTEGER,
-    "body" TEXT,
-    "date_written" BIGINT,
-    "asker_name" TEXT,
-    "asker_email" TEXT,
+    "product_id" INTEGER NOT NULL,
+    "body" TEXT NOT NULL,
+    "date_written" BIGINT NOT NULL,
+    "asker_name" TEXT NOT NULL,
+    "asker_email" TEXT NOT NULL,
     "reported" BOOLEAN NOT NULL DEFAULT false,
     "helpful" INTEGER NOT NULL DEFAULT 0,
 
@@ -15,11 +15,11 @@ CREATE TABLE "Questions" (
 -- CreateTable
 CREATE TABLE "Answers" (
     "id" SERIAL NOT NULL,
-    "question_id" INTEGER,
-    "body" TEXT,
-    "date_written" BIGINT,
-    "answerer_name" TEXT,
-    "answerer_email" TEXT,
+    "question_id" INTEGER NOT NULL,
+    "body" TEXT NOT NULL,
+    "date_written" BIGINT NOT NULL,
+    "answerer_name" TEXT NOT NULL,
+    "answerer_email" TEXT NOT NULL,
     "reported" BOOLEAN NOT NULL DEFAULT false,
     "helpful" INTEGER NOT NULL DEFAULT 0,
     "questionsId" INTEGER,
@@ -30,8 +30,8 @@ CREATE TABLE "Answers" (
 -- CreateTable
 CREATE TABLE "Photos" (
     "id" SERIAL NOT NULL,
-    "answer_id" INTEGER,
-    "url" TEXT,
+    "answer_id" INTEGER NOT NULL,
+    "url" TEXT NOT NULL,
 
     CONSTRAINT "Photos_pkey" PRIMARY KEY ("id")
 );
@@ -40,19 +40,22 @@ CREATE TABLE "Photos" (
 CREATE UNIQUE INDEX "Questions_id_key" ON "Questions"("id");
 
 -- CreateIndex
-CREATE INDEX "Questions_helpful_idx" ON "Questions"("helpful");
+CREATE INDEX "Questions_helpful_product_id_idx" ON "Questions"("helpful", "product_id");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "Answers_id_key" ON "Answers"("id");
 
 -- CreateIndex
-CREATE INDEX "Answers_helpful_answerer_name_idx" ON "Answers"("helpful", "answerer_name");
+CREATE INDEX "Answers_helpful_question_id_idx" ON "Answers"("helpful", "question_id");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "Photos_id_key" ON "Photos"("id");
+
+-- CreateIndex
+CREATE INDEX "Photos_answer_id_idx" ON "Photos"("answer_id");
 
 -- AddForeignKey
 ALTER TABLE "Answers" ADD CONSTRAINT "Answers_questionsId_fkey" FOREIGN KEY ("questionsId") REFERENCES "Questions"("id") ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "Photos" ADD CONSTRAINT "Photos_answer_id_fkey" FOREIGN KEY ("answer_id") REFERENCES "Answers"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+ALTER TABLE "Photos" ADD CONSTRAINT "Photos_answer_id_fkey" FOREIGN KEY ("answer_id") REFERENCES "Answers"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
